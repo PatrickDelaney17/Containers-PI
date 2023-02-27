20d8c059afa2f3311e893ee4e21bcbe503e04a9c
Patrick Delaney <delaney.patrick17@gmail.com>
2023-02-27 13:52:35 -0500

    update node container

--- ContainerSamples/node/server.js
+++ ContainerSamples/node/server.js
@@ -1,13 +1,33 @@
-const http = require("http");
-const gracefulShutdown = require("http-graceful-shutdown");
+'use strict';
 
-const server = http.createServer((req, res) => {
-    setTimeout(() => {
-      res.write("Server is running!");
-      res.end();
-    }, 20000);
-  });
+const express = require('express');
+
+// Constants
+const PORT = 3333;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
   
-  server.listen(3333);
-  
-  gracefulShutdown(server);
+    //get the request url following the port 
+    // req.url = /user
+    // to get the query string after it 
+    //  var q = url.parse(req.url, true).query;   // https://www.w3schools.com/nodejs/nodejs_http.asp
+
+  res.send('Server is up!');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+
+function detectReqValue() {
+    var currentQueryString = window.location.search;
+    if (currentQueryString) {
+        return true;
+    } else {
+      return false;
+    }
+}
